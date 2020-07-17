ae38ffa728cf92acd8439fb54cdac565a3b607a9
cjwcommuny <cjwcommuny@outlook.com>
2020-07-17 17:49:36 +0800

    change name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="torchtools",
+    name="torchtools-cjwcommuny",
     version="0.0.1",
     author="cjwcommuny",
     author_email="cjwcommuny@outlook.com",
