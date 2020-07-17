268eea64116a095505457dceca5bdc73fbf74fa1
cjwcommuny <cjwcommuny@outlook.com>
2020-07-17 21:53:21 +0800

    v0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="torchtools-cjwcommuny",
-    version="0.0.3",
+    version="0.0.4",
     author="cjwcommuny",
     author_email="cjwcommuny@outlook.com",
     description="Toolkit for Pytorch",
