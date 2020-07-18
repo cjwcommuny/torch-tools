08d915c2a18b71747b01dc69def9888e65611d0e
cjwcommuny <cjwcommuny@outlook.com>
2020-07-18 20:43:24 +0800

    add TensorGroup

--- torchtools/tensors/function.py
+++ torchtools/tensors/function.py
@@ -1,7 +1,6 @@
-from typing import Tuple
-
 import torch
 from torch import Tensor
+
 
 def value_in_tensor(value: Tensor, tensor: Tensor) -> bool:
     return value.view(1, -1).eq(tensor.view(-1, 1)).sum(0).eq(1).item()
@@ -14,3 +13,17 @@ def has_nan(x: Tensor) -> bool:
 
 def has_inf(x: Tensor) -> bool:
     return torch.isinf(x).any().item()
+
+def randbool(*size) -> Tensor:
+    return torch.rand(size=size) >= 0.5
+
+def randbool_like(
+        input
+) -> Tensor:
+    return randbool(*input.shape)
+
+def unsqueeze(input: Tensor, dim: int, num: int) -> Tensor:
+    new_shape = input.shape[:dim] + (1,) * num + input.shape[dim:]
+    return input.reshape(new_shape)
+
+
